0e98b1fb19e17180aa8345882fff22994df7cdd3
Rafael Diaz <rafaelmarzoratti02@gmail.com>
2023-01-13 17:12:23 -0300

    Diferenciando botões de funções, alterando css e adicionada função pressionaEnter

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,16 +3,40 @@ function criaCalculadora() {
     display: document.querySelector(".display"),
     btnClear: document.querySelector(".btn-clear"),
 
+    inicia() {
+      this.clickButtons()
+      this.pressionaEnter()
+    },
+    pressionaEnter() {
+      this.display.addEventListener("keyup", (e) => {
+        if (e.keyCode === 13) {
+          this.realizaConta()
+        }
+      })
+    },
+
+    realizaConta() {
+      let conta = this.display.value
+
+      try {
+        conta = eval(conta)
+        if (!conta) {
+          alert("Conta inválida")
+          return
+        }
+        this.display.value = conta
+      } catch (e) {
+        alert("Conta inválida")
+        return
+      }
+    },
+
     clearDisplay() {
       this.display.value = ""
     },
 
     apagaUm() {
       this.display.value = this.display.value.slice(0, -1)
-    },
-
-    inicia() {
-      this.clickButtons()
     },
 
     clickButtons() {
@@ -29,6 +53,10 @@ function criaCalculadora() {
 
         if (el.classList.contains("btn-del")) {
           this.apagaUm()
+        }
+
+        if (el.classList.contains("btn-eq")) {
+          this.realizaConta()
         }
       })
     },
