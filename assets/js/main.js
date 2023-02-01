44ce0447f9a83396b92d4353023558183fb9c6cb
Rafael Diaz <rafaelmarzoratti02@gmail.com>
2023-02-01 16:04:00 -0300

    Mudando de função fábrica para uma construtora e reduzindo linhas de código

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,71 +1,50 @@
-function criaCalculadora() {
-  return {
-    display: document.querySelector(".display"),
-    btnClear: document.querySelector(".btn-clear"),
+function Calculadora() {
+  this.display = document.querySelector(".display")
+  this.inicia = () => {
+    this.pegaCliques()
+    this.identificaEnter()
+  }
 
-    inicia() {
-      this.clickButtons()
-      this.pressionaEnter()
-    },
-    pressionaEnter() {
-      this.display.addEventListener("keyup", (e) => {
-        if (e.keyCode === 13) {
-          this.realizaConta()
-        }
-      })
-    },
+  this.identificaEnter = () => {
+    document.addEventListener("keypress", (e) => {
+      if (e.keyCode === 13) {
+        this.equals()
+      }
+    })
+  }
 
-    realizaConta() {
-      let conta = this.display.value
+  this.pegaCliques = () => {
+    document.addEventListener("click", (event) => {
+      const element = event.target
+      if (element.classList.contains("btn-num")) this.addNumero(element)
+      if (element.classList.contains("btn-clear")) this.clear()
+      if (element.classList.contains("btn-del")) this.delete()
+      if (element.classList.contains("btn-eq")) this.equals(element)
+    })
+  }
 
-      try {
-        conta = eval(conta)
-        if (!conta) {
-          alert("Conta inválida")
-          return
-        }
-        this.display.value = conta
-      } catch (e) {
+  this.addNumero = (element) => {
+    this.display.value += element.innerText
+    this.display.focus()
+  }
+  this.clear = () => (this.display.value = "")
+  this.delete = () => (this.display.value = this.display.value.slice(0, -1))
+  this.equals = () => {
+    try {
+      const conta = eval(this.display.value)
+
+      if (!conta) {
         alert("Conta inválida")
         return
       }
-    },
-
-    clearDisplay() {
-      this.display.value = ""
-    },
-
-    apagaUm() {
-      this.display.value = this.display.value.slice(0, -1)
-    },
-
-    clickButtons() {
-      document.addEventListener("click", (e) => {
-        const el = e.target
-
-        if (el.classList.contains("btn-num")) {
-          this.btnParaDisplay(el.innerText)
-        }
-
-        if (el.classList.contains("btn-clear")) {
-          this.clearDisplay()
-        }
-
-        if (el.classList.contains("btn-del")) {
-          this.apagaUm()
-        }
-
-        if (el.classList.contains("btn-eq")) {
-          this.realizaConta()
-        }
-      })
-    },
-
-    btnParaDisplay(valor) {
-      this.display.value += valor
-    },
+      this.display.value = conta
+    } catch (e) {
+      alert("Conta inválida")
+      return
+    }
   }
 }
 
-const calculadora = criaCalculadora()
+const calculadora = new Calculadora()
+
 calculadora.inicia()
